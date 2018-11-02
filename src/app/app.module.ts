e1dbeb77c84ec473da5498dfb6fa766830fa0932
Takoda Register <Takodr2@gmail.com>
2018-11-01 21:38:21 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
-import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeesComponent
+    EmployeesComponent,
+    EmployeeDetailComponent
   ],
   imports: [
     BrowserModule,
